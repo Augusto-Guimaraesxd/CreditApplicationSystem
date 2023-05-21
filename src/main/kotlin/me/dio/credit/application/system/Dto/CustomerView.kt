feab0840f788dde165c953eaf0df07c8c0171014
Perkles <augusto15guimaraes@gmail.com>
2023-05-20 22:01:57 -0300

    APP Complet

---
+++ src/main/kotlin/me/dio/credit/application/system/Dto/CustomerView.kt
@@ -0,0 +1,29 @@
+package me.dio.credit.application.system.Dto
+
+import me.dio.credit.application.system.entity.Customer
+import org.hibernate.validator.constraints.br.CPF
+import java.math.BigDecimal
+
+data class CustomerView (
+    val firstName: String,
+    val lastName: String,
+    val cpf: String,
+    val income: BigDecimal,
+    val email: String,
+    val zipCode: String,
+    val street: String
+
+){
+    constructor(customer:Customer): this(
+        firstName = customer.firstName,
+        lastName = customer.lastName,
+        cpf = customer.cpf,
+        email = customer.email,
+        zipCode = customer.adress.zipCode,
+        street = customer.adress.street,
+        income = customer.income
+    )
+}
+
+
+
